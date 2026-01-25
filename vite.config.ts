38fe36a9d33ec81f65201af358bc8627e15cc446
张向阳 <19049128297@163.com>
2026-01-25 16:43:24 +0800

    修改节点问题

--- vite.config.ts
+++ vite.config.ts
@@ -37,23 +37,7 @@ export default defineConfig({
           });
         },
       },
-      // "/bsc-rpc": {
-      //   target: "https://bsc-dataseed.binance.org",
-      //   changeOrigin: true,
-      //   secure: false,
-      //   rewrite: (path) => path.replace(/^\/bsc-rpc/, ""),
-      //   configure: (proxy, _options) => {
-      //     proxy.on('error', (err, _req, _res) => {
-      //       console.log('proxy error', err);
-      //     });
-      //     proxy.on('proxyReq', (proxyReq, req, _res) => {
-      //       console.log('Sending Request to the Target:', req.method, req.url);
-      //     });
-      //     proxy.on('proxyRes', (proxyRes, req, _res) => {
-      //       console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-      //     });
-      //   },
-      // }
+      
     },
   },
   build: {
