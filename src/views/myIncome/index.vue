98becb39dd7f12b60b5a11776bfe029934d05cff
Asadhcj <asadhcj@163.com>
2026-03-25 14:35:39 +0800

    feat:优化。

--- src/views/myIncome/index.vue
+++ src/views/myIncome/index.vue
@@ -600,7 +600,7 @@ const fetchNodeStakingInfo = async () => {
   data.id = currentNodeStakingInfo.value.id;
   data.name = t(nodeTypeMap[data.node_level]?.nodeNameKey || "");
   currentNodeStakingInfo.value = data;
-  currentNodeStakingInfo.value.produced_income = Number(currentNodeStakingInfo?.produced_income) > Number(currentNodeStakingInfo.value.forecast_income) ? currentNodeStakingInfo.value.forecast_income : currentNodeStakingInfo?.produced_income
+  currentNodeStakingInfo.value.produced_income = Number(currentNodeStakingInfo?.value.produced_income) > Number(currentNodeStakingInfo.value.forecast_income) ? currentNodeStakingInfo.value.forecast_income : currentNodeStakingInfo?.value.produced_income
   // 返回两个字段 已发放奖励  总奖励  计算百分比
   currentNodeStakingInfo.value.progressPercent =
     (1 - (Number(currentNodeStakingInfo.value.produced_income) /
